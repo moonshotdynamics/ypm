93911adb30c1b0a784ac911c4a6b2b0357e16eed
Isaac Tshiteta <moonshotdynamicsdev@gmail.com>
2024-09-11 09:55:58 +0200

    move hooks

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -13,7 +13,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
-import { useToast } from '@/hooks/use-toast';
+import { useToast } from '@/components/hooks/use-toast';
 
 
 export default function ContactUs() {
